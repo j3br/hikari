0cdb363f241e04c9e1b2b9f2eaef78b939510572
j3br <11167461+j3br@users.noreply.github.com>
2024-06-03 19:55:28 +0200

    chore: formatting

--- hikari/config.py
+++ hikari/config.py
@@ -1,23 +1,26 @@
-import os
-from dataclasses import dataclass, field
+from dataclasses import dataclass
+
 
 @dataclass
 class ILOAuth:
-    username: str = os.environ.get("ILO_USER")
-    password: str = os.environ.get("ILO_PASS")
+    username: str
+    password: str
+
 
 @dataclass
 class ILOSettings:
-    auth : ILOAuth = field(default_factory=ILOAuth)
-    base_url: str = os.environ.get("ILO_URL")
-    verify_ssl: bool = os.getenv('ILO_VERIFY_SSL', 'False') == 'True'
+    auth: ILOAuth
+    base_url: str
+    verify_ssl: bool
+
 
 @dataclass
 class DiscordSettings:
-    token: str = os.environ.get("DISCORD_TOKEN")
-    channel_id : str = os.environ.get("CHANNEL_ID")
+    token: str
+    channel_id: str
+
 
 @dataclass
 class Settings:
-    ilo: ILOSettings = field(default_factory=ILOSettings)
-    discord: DiscordSettings = field(default_factory=DiscordSettings)
+    ilo: ILOSettings
+    discord: DiscordSettings
