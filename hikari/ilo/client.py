652186a3446a427c8deed38d6e37922cc67f6bcf
j3br <11167461+j3br@users.noreply.github.com>
2024-06-03 20:15:53 +0200

    chore: formatting

--- hikari/ilo/client.py
+++ hikari/ilo/client.py
@@ -1,14 +1,13 @@
 import logging
 import requests
 from hikari.ilo import helpers
-from hikari.config import Settings
 
-settings = Settings()
 
 class APIClient:
-    def __init__(self, base_url, auth) -> None:
+    def __init__(self, base_url, auth, verify_ssl) -> None:
         self.base_url = base_url
         self.auth = auth
+        self.verify_ssl = verify_ssl
         self.actions = helpers.ActionsHelper(wrapper=self)
 
         self.logger = logging.getLogger("discord")
@@ -21,23 +20,26 @@ class APIClient:
             response = requests.request(
                 method,
                 url,
-                headers = {"Content-type": "application/json"},
+                headers={"Content-type": "application/json"},
                 auth=(self.auth.username, self.auth.password),
                 params=params,
                 data=data,
                 timeout=10,
-                verify=settings.ilo.verify_ssl
+                verify=self.verify_ssl,
             )
             response.raise_for_status()
+            return response
+
         except requests.exceptions.RequestException as exc:
             self.logger.error("An error occurred during the HTTP request: %s", exc)
 
-        return response
-
-
     def __getattr__(self, name):
         if name in ("get", "post", "put", "patch", "delete"):
+
             def method_wrapper(path, **kwargs):
                 return self.make_request(path, method=name.upper(), **kwargs)
+
             return method_wrapper
-        raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
+        raise AttributeError(
+            f"'{self.__class__.__name__}' object has no attribute '{name}'"
+        )
